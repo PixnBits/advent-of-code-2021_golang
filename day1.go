27a14291fe39d61c5620851589042f2fe7e27b5d
N Oliver <github@pixnbits.org>
2021-12-20 12:51:30 -0700

    feat(day1): first half

---
+++ day1.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+  "bufio"
+  "log"
+  "strconv"
+  "os"
+)
+
+func main() {
+  // fmt.Println("Hello, World!")
+  // for i := 0; i < 5; i++ {
+  //   defer fmt.Printf("%d ", i)
+  // }
+
+  deeperCount := 0
+  shallowerCount := 0
+
+  file, err := os.Open("./depths.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+
+  // scanner.Scan()
+  // previous, err := strconv.Atoi(scanner.Text())
+  // if err != nil {
+  //   log.Fatal(err)
+  // }
+
+  var previous int = 0
+
+  for scanner.Scan() {
+    lineDepth, err := strconv.Atoi(scanner.Text())
+    if err != nil {
+      log.Fatal(err)
+    }
+    // log.Print(lineDepth)
+
+    if previous == 0 {
+      previous = lineDepth
+      log.Printf("%v (N/A - no previous measurement)", lineDepth)
+      continue
+    }
+    // log.Println(lineDepth)
+    // log.Println(lineDepth)
+    if lineDepth < previous {
+      shallowerCount += 1
+      log.Printf("%v (decreased)", lineDepth)
+    } else if lineDepth > previous {
+      deeperCount += 1
+      log.Printf("%v (increased)", lineDepth)
+    } else {
+      log.Printf("%v (unchanged)", lineDepth)
+    }
+    previous = lineDepth
+  }
+
+  if err := scanner.Err(); err != nil {
+    log.Fatal(err)
+  }
+
+  log.Printf("summary: %v deepers, %v shallowers", deeperCount, shallowerCount)
+}
